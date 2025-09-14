5ac50c63223e45485ba161bc748b8c0f397384c7
k250067-cmd <k250067@nu.edu.pk>
2025-09-14 12:25:21 +0500

    Add files via upload

---
+++ PFlabs/SECOND.C
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+int main(){
+    printf("Name: HUSSAIN\n");
+    printf("Age: 19\n");
+    printf("Course: \"C Programming\"\n");
+    printf("Result: Passed\n");;
+    return 0;
+
+
+}
---
+++ PFlabs/first.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+int main(){
+float sub;
+float sub2;
+float sub3;
+float sum;
+float per;
+printf("give me MARKS OF subject1:\n");
+scanf("%f", &sub);
+printf("give me MARKS OF subject2:\n");
+scanf("%f", &sub2);
+printf("give me MARKS OF subject3:\n");
+scanf("%f", &sub);
+sum = sub + sub2 + sub3;
+per = (sum / 300) * 100;
+printf("the percentage %.2f ",per);
+return 0;
+}

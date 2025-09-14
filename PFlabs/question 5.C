7783b199551e6631c9e5a47c8eaa61a0c90942f0
k250067-cmd <k250067@nu.edu.pk>
2025-09-14 00:27:10 -0700

    Update and rename SECOND.C to question 5.C

---
+++ PFlabs/question 5.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+// Escape sequences in C (like \n, \t, \") are used to format text output. In this task, you will practice using
+// escape sequences.Write a C program that displays the following details exactly in the same format as shown
+// below:
+// Name: Ali
+// Age: 20
+// Course: "C Programming"
+// Result: Passed
+
+
+int main(){
+    printf("Name: ALI\n");
+    printf("Age: 20\n");
+    printf("Course: \"C Programming\"\n");
+    printf("Result: Passed\n");;
+    return 0;
